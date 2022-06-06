94a6d092bd922937230cc7d0758776fa99b18d5d
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2022-06-06 17:46:19 +0700

    I'm tired
    Co-Authored-By: David <nguyenhieucoder47@gmail.com>

--- src/Main.php
+++ src/Main.php
@@ -4,13 +4,19 @@ declare(strict_types=1);
 
 namespace NhanAZ\AntiServerStop;
 
-use pocketmine\utils\Config;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 use pocketmine\event\Listener;
+use pocketmine\lang\KnownTranslationFactory;
+use pocketmine\permission\DefaultPermissionNames;
+/* use pocketmine\permission\PermissionManager; */
 use pocketmine\plugin\PluginBase;
-use pocketmine\event\server\CommandEvent;
+use pocketmine\utils\Config;
+use function base64_encode;
+use function random_bytes;
+use function substr;
 
 class Main extends PluginBase implements Listener {
-
 	protected Config $config;
 
 	private function generatorPassword(): string {
@@ -18,32 +24,36 @@ class Main extends PluginBase implements Listener {
 		return $password;
 	}
 
+	protected function onLoad(): void {
+		$commandMap = $this->getServer()->getCommandMap();
+		$stopCommand = $commandMap->getCommand("stop");
+		$commandMap->unregister($stopCommand);
+	}
+
 	protected function onEnable(): void {
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 		$this->saveDefaultConfig();
-		$this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML, [
-			"password" => $this->generatorPassword(),
-			"usage" => "Usage: /stop <password>",
-			"wrongPassword" => "Wrong password!"
-		]);
+		$this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML, ["password" => $this->generatorPassword(), "wrongPassword" => "Wrong password!"]);
+		$command = $this->getServer()->getCommandMap()->getCommand("stop");
+		$command->setDescription(KnownTranslationFactory::pocketmine_command_stop_description());
+		/* $permission = PermissionManager::getInstance()->getPermission($command->getPermission()); */
+		/* $permission->setDescription(?); */
 	}
 
-	public function onCommandEvent(CommandEvent $event) {
-		$command = $event->getCommand();
-		$args = explode(" ", $command);
-		$sender = $event->getSender();
-		$stopCmd = "stop";
-		if (in_array($args[0], ["{$stopCmd}", "/{$stopCmd}", "./{$stopCmd}", "./pocketmine:{$stopCmd}"])) {
-			if (!isset($args[1])) {
-				$event->cancel();
-				$sender->sendMessage($this->config->get("usage", "Usage: /stop <password>"));
-				return;
+	public function onCommand(CommandSender $sender, Command $command, string $commandLabel, array $args): bool {
+		if ($command->getName() === "stop") {
+			/* DefaultPermissionNames::COMMAND_STOP */
+			if (!isset($args[0])) {
+				return false;
 			}
-			if ($args[1] !== $this->config->get("password")) {
-				$event->cancel();
+			if ($args[0] !== $this->config->get("password")) {
 				$sender->sendMessage($this->config->get("wrongPassword", "Wrong password!"));
-				return;
+				return true;
 			}
+			Command::broadcastCommandMessage($sender, KnownTranslationFactory::commands_stop_start());
+			$sender->getServer()->shutdown();
+			return true;
 		}
+		return false;
 	}
 }
